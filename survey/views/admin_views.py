27f0896e137eb9546d952f0bd54ac6537a69ced5
Enzo Hilzinger <enzo.hilzinger@sap.com>
2019-08-29 02:47:15 +0200

    fixed pw encryption

--- survey/views/admin_views.py
+++ survey/views/admin_views.py
@@ -7,9 +7,11 @@ from rest_framework.response import Response
 
 from ..serializers import UserSerializer
 
+
 class GetUserDetailView(generics.RetrieveAPIView):
     def get(self, request, *args, **kwargs):
         return Response(data={'username': request.user.username}, status=status.HTTP_200_OK)
+
 
 class RegisterUserView(generics.ListCreateAPIView):
     permission_classes = (permissions.AllowAny,)
@@ -17,8 +19,6 @@ class RegisterUserView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data={'message': 'Created User'}, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        User.objects.create_user(
+            username=request.data['username'], password=request.data['password'], email=request.data['email'])
+        return Response(data={'message': 'Created User'}, status=status.HTTP_201_CREATED)
